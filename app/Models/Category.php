717b6f6a5b57a70337097960aec840b2e166e9ae
Baraaabudraz <abudrazbaraa@gmail.com>
2024-04-16 16:17:02 +0300

    Update Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\helpers;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
@@ -10,6 +11,7 @@ use function Termwind\renderUsing;
 class Category extends Model
 {
     use HasTranslations;
+    use helpers;
     protected $translatable  = ['name','description'];
 
     use HasFactory;
